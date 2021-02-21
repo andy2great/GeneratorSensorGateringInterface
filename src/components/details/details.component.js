799e7460afd88675b98f79c3208241c18f2628d9
andy2great <andy2greatu2@gmail.com>
2021-02-20 19:16:44 -0500

    adding chart

--- src/components/details/details.component.js
+++ src/components/details/details.component.js
@@ -1,23 +1,20 @@
-import './details.style.css';
+import "./details.style.css";
 
-import { Button } from '@material-ui/core';
+import { Button } from "@material-ui/core";
 
 export const Details = (props) => {
-
-    return (
-        <div className='parent'>
-            <Button 
-                css='button'
-                color='primary'
-                variant='contained'
-                onClick={() => props.handler(null)}
-            >
-                Retour
-            </Button>
-            allo papa
-            <div className='enfant'>
-                comment ca va
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="parent">
+      <Button
+        css="button"
+        color="primary"
+        variant="contained"
+        onClick={() => props.handler(null)}
+      >
+        Retour
+      </Button>
+      allo papa
+      <div className="enfant">comment ca va</div>
+    </div>
+  );
+};
--- src/components/module-content/module-content.component.js
+++ src/components/module-content/module-content.component.js
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { Summary } from '../summary/summary.component';
-import { Details } from '../details/details.component';
+import { Summary } from "../summary/summary.component";
+import { Details } from "../details/details.component";
 
 export const ModuleContent = () => {
-    const [module, setModule] = useState();
+  const [module, setModule] = useState();
 
-    const handleModuleClick = (module) => {
-        setModule(module);
-    }
+  const handleModuleClick = (module) => {
+    setModule(module);
+  };
 
-    return (
-        <div>
-            {module ?
-                (<Details handler={handleModuleClick}></Details>) :
-                (<Summary handler={handleModuleClick}></Summary>)
-            }
-        </div>
-    )
-}
+  return (
+    <div>
+      {module ? (
+        <Details handler={handleModuleClick}></Details>
+      ) : (
+        <Summary handler={handleModuleClick}></Summary>
+      )}
+    </div>
+  );
+};
